5acc270b4052bdce94d6b2e502401fdda8107380
Tyler Silva <tylerbsilva@gmail.com>
2015-06-09 16:04:14 -0500

    Added a physics engine to make bird fall

--- site/js/components/graphics/pipe.js
+++ site/js/components/graphics/pipe.js
@@ -2,10 +2,14 @@ var PipeGraphicsComponent = function(entity) {
     this.entity = entity;
 };
 
-PipeGraphicsComponent.prototype.draw = function(context) {
-    context.fillStyle = "green";
-    context.rect(100, 100, 100,100);
-    context.fill();
+PipeGraphicsComponent.prototype.draw = function(context, position, size) {
+  context.save();
+  context.translate(position.x, position.y);
+  context.scale(size, size);
+  context.beginPath();
+  context.rect(0, 0, 0, 0);
+  context.fill();
+  context.restore();
 };
 
 exports.PipeGraphicsComponent = PipeGraphicsComponent;
--- site/js/flappy_bird.js
+++ site/js/flappy_bird.js
@@ -1,14 +1,18 @@
 var graphicsSystem = require('./systems/graphics');
+var physicsSystem = require('./systems/physics');
+
 var bird = require('./entities/bird');
-var pipe = require('./entities/pipe');
+//var pipe = require('./entities/pipe');
 
 var FlappyBird = function() {
-    this.entities = [new bird.Bird(), new pipe.Pipe()];
-    this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
+  this.entities = [new bird.Bird()];
+  this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
+  this.physics = new physicsSystem.PhysicsSystem(this.entities);
 };
 
 FlappyBird.prototype.run = function() {
-    this.graphics.run();
+  this.graphics.run();
+  this.physics.run();
 };
 
 exports.FlappyBird = FlappyBird;
