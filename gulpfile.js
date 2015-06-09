56173f440e0bf5cc192c24da32d6486218a72986
Tyler Silva <tylerbsilva@gmail.com>
2015-06-09 11:37:10 -0500

    Setup gulp file and organized folders

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var livereload = require('gulp-livereload');
+
+// Compile Sass task
+gulp.task('sass', function() {
+  return gulp.src('site/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('site/css'));
+});
+
+// Watch task
+gulp.task('watch', function() {
+  gulp.watch('site/js/*.js', ['jshint']);
+  gulp.watch('site/scss/*.scss', ['sass']);
+  // Create LiveReload server
+  livereload.listen();
+  // Watch any files in dist/, reload on change
+  gulp.watch(['site/css/**']).on('change', livereload.changed);
+});
+
+// Minify index
+gulp.task('html', function() {
+  gulp.src('site/index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('build/'));
+});
+
+// JavaScript build task, removes whitespace and concatenates all files
+gulp.task('scripts', function() {
+  return browserify('./site/js/main.js')
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+// Styles build task, concatenates all the files
+gulp.task('styles', function() {
+  gulp.src('site/css/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+
+// Default task
+gulp.task('default', ['sass', 'watch']);
