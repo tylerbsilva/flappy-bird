c8584ad0c860507e2f43e88219c44e3a646d00f9
Tyler Silva <tylerbsilva@gmail.com>
2015-06-09 12:47:01 -0500

    Bird entity is now a circle

--- site/js/components/graphics/bird.js
+++ site/js/components/graphics/bird.js
@@ -2,8 +2,10 @@ var BirdGraphicsComponent = function(entity) {
     this.entity = entity;
 };
 
-BirdGraphicsComponent.prototype.draw = function() {
-    console.log("Drawing a bird");
+BirdGraphicsComponent.prototype.draw = function(context) {
+    context.beginPath();
+    context.arc(50, 50, 10, 0, 2 * Math.PI);
+    context.fill();
 };
 
 exports.BirdGraphicsComponent = BirdGraphicsComponent;
