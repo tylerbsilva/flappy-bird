c7d0f508e0a8d09a01ad45e2dcd532e2d28afe9d
Tyler Silva <tylerbsilva@gmail.com>
2015-06-16 15:11:37 -0500

    Fixing touch support

--- site/js/systems/input.js
+++ site/js/systems/input.js
@@ -1,18 +1,25 @@
 var InputSystem = function(entities) {
   this.entities = entities;
-
   // Canvas is where we get input from
   this.canvas = document.getElementById('main-canvas');
 };
 
 InputSystem.prototype.run = function() {
-    this.canvas.addEventListener('click', this.onClick.bind(this));
-    this.canvas.addEventListener('tap', this.onClick.bind(this));
+  this.canvas.addEventListener('click', this.onClick.bind(this));
+  this.canvas.addEventListener('touchstart', this.onTouch.bind(this));
 };
 
 InputSystem.prototype.onClick = function() {
-    var bird = this.entities[2];
-    bird.components.physics.velocity.y = 0.6;
+  console.log('click');
+  var bird = this.entities[2];
+  bird.components.physics.velocity.y = 0.6;
+};
+
+InputSystem.prototype.onTouch = function(e) {
+  console.log('touch');
+  e.preventDefault();
+  var bird = this.entities[2];
+  bird.components.physics.velocity.y = 0.6;
 };
 
 exports.InputSystem = InputSystem;
