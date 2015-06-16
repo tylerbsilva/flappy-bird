f24323b747d3109bb5519b9418b2f684c1a942f1
Tyler Silva <tylerbsilva@gmail.com>
2015-06-16 12:04:19 -0500

    Added a high score feature

--- site/js/systems/score.js
+++ site/js/systems/score.js
@@ -2,19 +2,31 @@ var pipeTop = require('../entities/pipeTop');
 
 var ScoreSystem = function() {
   // span that we're updating
-  this.span = document.getElementById('score');
+  this.scoreSpan = document.getElementById('score');
+  this.highSpan = document.getElementById('high-score');
   // current score
   this.userScore = 0;
+  this.highScore = 0;
 };
 
 ScoreSystem.prototype.addOne = function() {
+  // Add to score
   this.userScore += 1;
-  this.span.innerHTML = this.userScore;
+  // Update HTML
+  this.scoreSpan.innerHTML = this.userScore;
 };
 
 ScoreSystem.prototype.reset = function() {
+  // Chcek to see if you beat the high score
+  if(this.userScore > this.highScore){
+    this.highScore = this.userScore;
+    this.highSpan.innerHTML = this.highScore;
+  }
+  // Set score back to zero
   this.userScore = 0;
-  this.span.innerHTML = this.userScore;
+  // Update HTML
+  this.scoreSpan.innerHTML = this.userScore;
 };
+
 
 exports.ScoreSystem = ScoreSystem;
